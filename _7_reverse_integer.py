186f2fa822f81a615f2d72186ccacf78702b668c
chasingw <cchasingw@gmail.com>
2019-07-29 22:34:17 +0800

    um swp file

--- _7_reverse_integer.py
+++ _7_reverse_integer.py
@@ -1,4 +1,17 @@
 '''
 Given a 32-bit signed integer, reverse digits of an integer.
+123 - 321
+-123
+-321
+120
+21
 '''
+class Solution:
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        x_str = str(x)
+        
 
